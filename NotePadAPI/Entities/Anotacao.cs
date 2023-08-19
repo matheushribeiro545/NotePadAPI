3bc79013367791af405b480067a444e0398aed4b
Matheus Ribeiro <matheusribeiro545@gmail.com>
2023-08-19 12:54:24 -0300

    Criação da classe Anotação

--- NotePadAPI/Entities/Anotacao.cs
+++ NotePadAPI/Entities/Anotacao.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace NotePadAPI.Entities
 {
@@ -11,10 +12,14 @@ namespace NotePadAPI.Entities
 	public class Anotacao
 	{
 		[Key]
-		public int IdAnotacao { get; set; }
-		public string DescricaoNota { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdAnotacao { get; set; }
+		public string Titulo { get; set; }
+		public string Mensagem { get; set; }
 		public DateTime DataInclusao { get; set; }
-		public DateTime DataAlteracao { get; set; }
+		public DateTime? DataAlteracao { get; set; }
+		public int IdUsuario { get; set; }
+		public bool Ativo { get; set; }
 	}
 }
 
